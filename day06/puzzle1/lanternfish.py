0b88cb4d1920a401c2f596e0447b7697cdac2135
Rodrigue A <rodrigue@lingokids.com>
2021-12-07 09:14:38 +0100

    Add solution for day 6 / puzzle 1
    Using brute force for now.

---
+++ day06/puzzle1/lanternfish.py
@@ -0,0 +1,23 @@
+import sys
+
+from collections import defaultdict
+
+
+def main(args):
+    input_file = "test_input.txt" if len(args) > 1 else "input.txt"
+    with open(input_file, "r") as f:
+        fishes = [int(n) for n in f.readline().strip().split(",")]
+    for i in range(256):
+        for i in range(len(fishes)):
+            if fishes[i] == 0:
+                fishes[i] = 6
+                fishes.append(8)
+            else:
+                fishes[i] -= 1
+
+    print(len(fishes))
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
