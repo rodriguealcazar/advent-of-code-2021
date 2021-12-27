1709dadf7da311f177e7b2e8cdd24a06acab4f0a
Rodrigue A <rodrigue@lingokids.com>
2021-12-27 14:31:33 +0100

    Use same counter for 0s and 1s
    Instead of counting each separately and comparing we use the same
    counter for boths.

--- day03/puzzle1/diagnostic.py
+++ day03/puzzle1/diagnostic.py
@@ -1,30 +1,34 @@
+import sys
+
 from itertools import starmap
 
-ones = []
-zeros = []
+totals = []
 
 def count(index, bit):
-    i = int(bit)
-
-    if len(ones) == index:
-        ones.append(0)
-        zeros.append(0)
+    if len(totals) == index:
+        totals.append(0)
 
     if bit == 0:
-        zeros[index] += 1
+       totals[index] -= 1
     else:
-        ones[index] += 1
+       totals[index] += 1
 
-with open("input.txt", "r") as f:
-    for reading in f:
-        bits = list(map(int, reading.strip()))
-        list(starmap(count, list(enumerate(bits))))
 
-gamma_bin = [1 if ones[i] > zeros[i] else 0 for i in range(len(ones))]
-gamma = int(''.join(map(str, gamma_bin)), 2)
-epsilon_bin = [1 if zeros[i] > ones[i] else 0 for i in range(len(ones))]
-epsilon = int(''.join(map(str, epsilon_bin)), 2)
+def main(input_file):
+    with open(input_file, "r") as f:
+        for reading in f:
+            bits = list(map(int, reading.strip()))
+            list(starmap(count, list(enumerate(bits))))
 
-print(f"gamma: {gamma}")
-print(f"epsilon: {epsilon}")
-print(gamma * epsilon)
+    gamma_bin = [1 if totals[i] > 0 else 0 for i in range(len(totals))]
+    gamma = int(''.join(map(str, gamma_bin)), 2)
+    epsilon_bin = [1 if totals[i] < 0 else 0 for i in range(len(totals))]
+    epsilon = int(''.join(map(str, epsilon_bin)), 2)
+
+    print(f"gamma: {gamma}")
+    print(f"epsilon: {epsilon}")
+    print(gamma * epsilon)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
